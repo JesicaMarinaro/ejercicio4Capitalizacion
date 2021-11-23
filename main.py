b91031ebe558a5b2bdef082c2fe83e0256dacb7b
Jesica Marinaro <jesicamarinaro@hotmail.com>
2021-11-22 21:23:17 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# 4 Escribir una funcion que reciba un string con nombre y apellido
+# y devuelva un string con el nombre y apellido pero con capitalizacion
+# (primer letra en mayuscula)
+
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+cad = nombre + apellido
+print(cad.title())
+
